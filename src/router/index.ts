ab4159acb846115e3861fefc07706ab92a25d82b
Gugle <administration@ztxy666.cn>
2024-09-01 19:58:46 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,5 @@
 import {createRouter, createWebHashHistory, Router} from 'vue-router';
 import MainPage from '../views/MainPage.vue';
-import ImageViewPage from '../views/ImageViewPage.vue';
-import AboutPage from '../views/AboutPage.vue';
-import SupportPage from '../views/SupportPage.vue';
-import SkylandPage from '../views/additional/SkylandPage.vue';
 
 const router: Router = createRouter({
     history: createWebHashHistory('/web'),
